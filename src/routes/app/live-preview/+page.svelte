eda60c6ab41be65f14832fdb911a4b3b3d7b8db8
Zerebos <me@zerebos.com>
2026-06-14 00:47:26 -0400

    experiment with xterm and wterm

--- src/routes/app/live-preview/+page.svelte
+++ src/routes/app/live-preview/+page.svelte
@@ -2,9 +2,13 @@
     import Page from "$lib/views/Page.svelte";
     import LivePreview from "$lib/views/LivePreview.svelte";
     import InteractiveTerminal from "$lib/views/InteractiveTerminal.svelte";
+    import InteractiveTerminalWterm from "$lib/views/InteractiveTerminalWterm.svelte";
+    import InteractiveTerminalXterm from "$lib/views/InteractiveTerminalXterm.svelte";
 </script>
 
 <Page title="Live Preview">
     <LivePreview />
     <InteractiveTerminal />
+    <InteractiveTerminalWterm />
+    <InteractiveTerminalXterm />
 </Page>
